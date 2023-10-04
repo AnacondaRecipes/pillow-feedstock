b4105576ad171674a16eb5eedfeefa756bb742cb
Serhii Kupriienko <skupriienko@anaconda.com>
2023-10-04 11:42:26 +0300

    Add openjpeg to enable jpeg2000 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export JPEG_ROOT=$PREFIX
-# export JPEG2K_ROOT=None
+export JPEG2K_ROOT=$PREFIX
 export ZLIB_ROOT=$PREFIX
 # export IMAGEQUANT_ROOT=None
 export TIFF_ROOT=$PREFIX
@@ -9,4 +9,4 @@ export FREETYPE_ROOT=$PREFIX
 export LCMS_ROOT=$PREFIX
 export WEBP_ROOT=$PREFIX
 
-$PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir --global-option="build_ext" --global-option="--enable-webp" -vvv
+$PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir --global-option="build_ext" --global-option="--enable-webp" --global-option="--enable-jpeg2000" -vv
