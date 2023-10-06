887bb1a77f661313f566ef3fad256efe260ef28e
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-10-06 11:13:06 +0300

    [PKG-2296] pillow 10.0.1 (#20)
    * pillow_10.0.1
    
    * Add openjpeg to enable jpeg2000 support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 set LIBRARY_DIRS=%LIBRARY_BIN%;%LIBRARY_LIB%
 
 set JPEG_ROOT=%LIBRARY_PREFIX%
-:: set JPEG2K_ROOT=%LIBRARY_PREFIX%
+set JPEG2K_ROOT=%LIBRARY_PREFIX%
 set ZLIB_ROOT=%LIBRARY_PREFIX%
 :: set IMAGEQUANT_ROOT=%LIBRARY_PREFIX%
 set TIFF_ROOT=%LIBRARY_PREFIX%
@@ -12,5 +12,5 @@ set FREETYPE_ROOT=%LIBRARY_PREFIX%
 set WEBP_ROOT=%LIBRARY_PREFIX%
 
 
-%PYTHON% -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir --global-option="build_ext" --global-option="--enable-webp" -vvv
+%PYTHON% -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir --global-option="build_ext" --global-option="--enable-webp" --global-option="--enable-jpeg2000" -vv
 if errorlevel 1 exit 1
