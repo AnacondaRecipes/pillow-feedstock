5be4623a27d2bc50683e02bc3078ddd2ce929eba
Steven <scrass@anaconda.com>
2025-01-22 10:53:11 -0700

    revert build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,19 +3,10 @@
 export JPEG_ROOT=$PREFIX
 export JPEG2K_ROOT=$PREFIX
 export ZLIB_ROOT=$PREFIX
-# currently disabled, see meta.yaml
-# export IMAGEQUANT_ROOT=$PREFIX
+# export IMAGEQUANT_ROOT=None
 export TIFF_ROOT=$PREFIX
 export FREETYPE_ROOT=$PREFIX
-# export FRIBIDI_ROOT=$PREFIX
 export LCMS_ROOT=$PREFIX
 export WEBP_ROOT=$PREFIX
-export XCB_ROOT=$PREFIX
 
-# add --vendor-raqm to installation (cannot be passed through pip install)
-echo "[build_ext]" >> setup.cfg
-echo "vendor-raqm=1" >> setup.cfg
-# sanity check
-cat setup.cfg
-
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+$PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir --global-option="build_ext" --global-option="--enable-webp" --global-option="--enable-jpeg2000" -vv
