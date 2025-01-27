ce73597cbb33cb45c1b7122967321bb549e398b2
Eric Lundby <Eric.Lundby@gmail.com>
2025-01-27 08:19:28 -0700

    Revert "Try setting LD_LIBRARY_PATCH"
    This reverts commit 809bbe578dafb5f07a9a52b00a84ee80be74156c.

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,4 @@ export FREETYPE_ROOT=$PREFIX
 export LCMS_ROOT=$PREFIX
 export WEBP_ROOT=$PREFIX
 
-export DYLD_LIBRARY_PATH=${PREFIX}/lib:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${PREFIX}/lib:$LD_LIBRARY_PATH
-
 $PYTHON -m pip install . --no-deps --no-build-isolation --ignore-installed --no-cache-dir --global-option="build_ext" --global-option="--enable-webp" --global-option="--enable-jpeg2000" -vv
