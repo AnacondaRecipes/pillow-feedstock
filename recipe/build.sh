5549e6cd37a99596c7cd47d0a56cdccaff1dacb7
Patryk Kups <pkups@anaconda.com>
2026-01-26 11:44:03 +0100

    check

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,13 +11,4 @@ export FREETYPE_ROOT=$PREFIX
 export LCMS_ROOT=$PREFIX
 export WEBP_ROOT=$PREFIX
 
-export PILLOW_ENABLE_LCMS=1
-export PILLOW_ENABLE_WEBP=1
-export PILLOW_ENABLE_JPEG2000=1
-export PILLOW_ENABLE_ZLIB=1
-export PILLOW_ENABLE_JPEG=1
-export PILLOW_ENABLE_TIFF=1
-export PILLOW_ENABLE_FREETYPE=1
-export PILLOW_ENABLE_WEBPMUX=1
-
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation --ignore-installed --no-cache-dir
